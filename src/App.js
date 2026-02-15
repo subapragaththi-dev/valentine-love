2fa17be74779b8a26a39ad42a571c1143e397594
Subash <subapragaththi@gmail.com>
2026-02-15 23:46:01 +0530

    temp rename

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Valentine from "./pages/Valentine";
-import Photos from "./pages/Photos";
+import Photos from "./pages/Temp";
 import CollagePage from "./pages/CollagePage";
 import EnvelopePage from "./pages/EnvelopePage";
 
